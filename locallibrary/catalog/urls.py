af966c76ac8944cd19510048282031a4bec43b0e
Paul Davis <pjdavis@gmx.com>
2017-12-31 18:27:35 +0100

    add tests

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     re_path(r'^books/$', views.BookListView.as_view(), name='books'),
     re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
-    #re_path(r'^book/<int:pk>)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
 
 urlpatterns += [
