0fe63b82cc2ff411ec740040282b4f38f0da3dfc
Paul Davis <pjdavis@gmx.com>
2018-01-20 00:05:19 +0100

    decompose model to reusable mixins

--- animal-mxins/animals/settings.py
+++ animal-mxins/animals/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'animals.apps.AnimalConfig',
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
--- animal-mxins/app/models.py
+++ animal-mxins/app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(Person, on_delete='CASCADE')
+    # ......
+    born = models.DateTimeField()
+    acquired = models.DateTimeField()
+
+
+class Ownable(models.Model):
+    owner = models.ForeignKey(Person, on_delete='CASCADE')
+
+    class Meta:
+        abstract = True
+
+class Ageable(models.Model):
+    born = models.DateTimeField()
+    acquired = models.DateTimeField()
+
+    @property
+    def age(self):
+        if self.born:
+            td = date.today() - self.born
+            return td.days/365
+
+    @property
+    def owned_for(self):
+        return (datetime.now() - self.acquired).days
+
+
+
+
+
+# from datetime import date
+# birthday = date(year=2005, month=11, day=1)
+
+
