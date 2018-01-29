083bd73f5a31184481f6a14b033d02dae7f1c87c
Paul Davis <pjdavis@gmx.com>
2018-01-29 23:30:31 +0100

    df

--- user_authentication/authenticate/urls.py
+++ user_authentication/authenticate/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('user_example.urls'))
+    path('', include('user_example.urls')),
     #gives urls that can use for a user management system
-    path('accounts/'), include('django.contrib.auth.urls')
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
